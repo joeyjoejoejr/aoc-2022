dd91d9b81e9dba0e6e8117011501fdcc4b42b93a
Joe Jackson <cpmhjoe@gmail.com>
2022-12-03 15:55:20 -0500

    Day 3 -- Part 2

--- src/day03.c
+++ src/day03.c
@@ -12,27 +12,32 @@ char input[] = "vJrwpWtwJgWrhcsFMMfFFhFp\n"
 #endif
 #include "shared/input.c"
 
-void splitRucksack(char *rucksack, char **firstCompartment, char **secondCompartment) {
+void splitRucksack(char *rucksack, char *firstCompartment, char *secondCompartment) {
   size_t len = strlen(rucksack);
-  *firstCompartment = strndup(rucksack, len / 2);
-  *secondCompartment = strdup(rucksack + len / 2);
+  char *end = stpncpy(firstCompartment, rucksack, len / 2);
+  *end = '\0';
+  strcpy(secondCompartment, rucksack + len / 2);
 }
 
-char getIntersection(char *firstCompartment, char *secondCompartment) {
-  size_t len = strlen(firstCompartment);
+void getIntersection(char *intersection, char *str1, char *str2) {
+  size_t counter = 0,
+         firstLen = strlen(str1),
+         secondLen = strlen(str2);
   char firstSparse['z' + 1] = {0},
        secondSparse['z' + 1] = {0};
 
-  for(size_t i = 0; i < len; ++i) {
-    firstSparse[(size_t)firstCompartment[i]] = 1;
-    secondSparse[(size_t)secondCompartment[i]] = 1;
-  }
+  for(size_t i = 0; i < firstLen; ++i)
+    firstSparse[(size_t)str1[i]] = 1;
+  for(size_t i = 0; i < secondLen; ++i)
+    secondSparse[(size_t)str2[i]] = 1;
 
   for(char i = 'A'; i <= 'z'; ++i) {
-    if(firstSparse[(size_t)i] && secondSparse[(size_t)i]) return i;
+    if(firstSparse[(size_t)i] && secondSparse[(size_t)i]) {
+      intersection[counter++] = i;
+    }
   }
 
-  return 0;
+  intersection[counter] = '\0';
 }
 
 size_t charToPriority(char item) {
@@ -44,13 +49,16 @@ size_t charToPriority(char item) {
 }
 
 int main(void) {
-  size_t total = 0;
-  char intersection,
-       *buffer,
+  size_t counter = 0,
+         total = 0,
+         part2Total = 0;
+  char *buffer,
        *rucksack,
-       *firstCompartment,
-       *secondCompartment,
        *sep = "\n";
+  char intersection[256],
+       firstCompartment[256],
+       secondCompartment[256];
+  char group[3][256];
 
 
   if(getInput(&buffer, "day03") <= 0) {
@@ -60,18 +68,25 @@ int main(void) {
 
   rucksack = strtok(buffer, sep);
   while(rucksack) {
-    splitRucksack(rucksack, &firstCompartment, &secondCompartment);
-    intersection = getIntersection(firstCompartment, secondCompartment);
-    total += charToPriority(intersection);
+    strcpy(group[counter % 3], rucksack);
+    if (counter % 3 == 2) {
+      getIntersection(intersection, group[0], group[1]);
+      getIntersection(intersection, intersection, group[2]);
+      part2Total += charToPriority(intersection[0]);
+    }
+
+    splitRucksack(rucksack, firstCompartment, secondCompartment);
+    getIntersection(intersection, firstCompartment, secondCompartment);
+    total += charToPriority(intersection[0]);
 
     rucksack = strtok(NULL, sep);
+    counter++;
   }
 
 
   printf("Part 1: %zu\n", total);
+  printf("Part 2: %zu\n", part2Total);
 
   if (buffer) free(buffer);
-  if (firstCompartment) free(firstCompartment);
-  if (secondCompartment) free(secondCompartment);
   return 0;
 }
